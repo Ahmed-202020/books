4ac01cceebba0b02a392ccb3b79b93b4e4faa884
Ahmed khaled <ak812554@gmail.com>
2021-03-23 21:06:12 +0200

    The main page Is completed

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import "@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css";
+import "./assets/sass/style.scss";
+import 'bootstrap/dist/js/bootstrap.min.js';
+import "jquery/dist/jquery.min.js";
+import 'popper.js/dist/popper.min.js';
+import "@fortawesome/fontawesome-free/js/all.min.js";
+import "normalize.css/normalize.css";
+
+$("#year").text(new Date().getFullYear());
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() >= 100) {
+
+        $("nav").addClass("scroll");
+    }
+    else {
+        $("nav").removeClass("scroll");
+    }
+});
---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'app'),
+        filename: 'app.js',
+        publicPath: '/',
+      },
+      module: {
+        rules: [
+          {
+            test: /\.html$/i,
+            loader: 'html-loader',
+          },
+          {
+            test: /\.(css|scss|sass)$/i,
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader",
+                "postcss-loader",
+                "sass-loader"
+            ],
+          },
+          {
+            test: /\.(ttf|woff|woff2|svg|eot)$/i,
+            exclude:/images/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: 'assets/fonts',
+                },
+              },
+            ],
+          },
+          {
+            test: /\.(png|jpe?g|gif|svg)$/i,
+            exclude:/fonts/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: 'assets/images',
+                },
+              },
+            ],
+          },
+          {
+            test: require.resolve("jquery"),
+            loader: "expose-loader",
+            options: {
+              exposes: ["$", "jQuery"],
+            },
+          },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "./src/index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "assets/css/style.css"
+        }),
+        new CleanWebpackPlugin({
+          cleanStaleWebpackAssets: false,
+        }),
+        new OptimizeCssAssetsPlugin({}),
+    ],
+    devServer: {
+        compress: true,
+        port: 9000,
+        open: true,
+        writeToDisk: true,
+        stats: 'errors-only',
+    },
+  };
+  
